90bedd12dbadd37a6bc70f4429b4f43d07394924
afyqeyzar <norafyqe@gmail.com>
2024-10-20 12:24:33 +0800

    feat: add sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
+import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+const futura = localFont({
+  src: "../fonts/futura.woff",
+  variable: "--font-geist-sans",
+  weight: "400",
+});
 
 export const metadata: Metadata = {
   title: "Dog website",
@@ -23,10 +19,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-      >
-        {children}
+    <html lang="en" className={futura.className}>
+      <body className="grid grid-cols-6">
+        <aside className="bg-yellow-200 p-4 h-screen col-span-1 flex flex-col gap-4">
+          <h2 className="text-6xl">Dog website</h2>
+
+          <ul className="flex flex-col gap-2">
+            <li className="text-2xl cursor-pointer hover:underline">
+              <Link href="/">Home page</Link>
+            </li>
+            <li className="text-2xl cursor-pointer hover:underline">
+              <Link href="/login">Login/Register</Link>
+            </li>
+          </ul>
+        </aside>
+        <div className="col-span-5 max-h-screen overflow-scroll p-4">
+          {children}
+        </div>
       </body>
     </html>
   );
